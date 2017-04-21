6e2036546148584e0f92d38280dede1105270832
Shige Takeda <smtakeda@gmail.com>
2017-04-20 22:46:36 -0700

    Added dsn_test

---
+++ dsn_test.go
@@ -0,0 +1,122 @@
+package gosnowflake
+
+import (
+	"testing"
+)
+
+type parseDsnTests struct {
+	dsn    string
+	config *Config
+	err    error
+}
+
+func TestParseDSN(t *testing.T) {
+	testcases := []parseDsnTests{
+		{
+			dsn: "user:pass@account",
+			config: &Config{
+				Account:  "account", User: "user", Password: "pass",
+				Protocol: "https", Host: "account.snowflakecomputing.com", Port: 443,
+			},
+			err: nil,
+		},
+		{
+			dsn: "user:pass@account/db",
+			config: &Config{
+				Account:  "account", User: "user", Password: "pass",
+				Protocol: "https", Host: "account.snowflakecomputing.com", Port: 443,
+				Database: "db", Schema: "public",
+			},
+			err: nil,
+		},
+		{
+			dsn: "user:pass@host:123/db/schema?account=ac&protocol=http",
+			config: &Config{
+				Account:  "ac", User: "user", Password: "pass",
+				Protocol: "http", Host: "host", Port: 123,
+				Database: "db", Schema: "schema",
+			},
+			err: nil,
+		},
+		{
+			dsn: "user@host:123/db/schema?account=ac&protocol=http",
+			config: &Config{
+				Account:  "ac", User: "user", Password: "pass",
+				Protocol: "http", Host: "host", Port: 123,
+				Database: "db", Schema: "schema",
+			},
+			err: ErrEmptyPassword,
+		},
+		{
+			dsn: "@host:123/db/schema?account=ac&protocol=http",
+			config: &Config{
+				Account:  "ac", User: "user", Password: "pass",
+				Protocol: "http", Host: "host", Port: 123,
+				Database: "db", Schema: "schema",
+			},
+			err: ErrEmptyUsername,
+		},
+		{
+			dsn: "user:p@host:123/db/schema?protocol=http",
+			config: &Config{
+				Account:  "ac", User: "user", Password: "pass",
+				Protocol: "http", Host: "host", Port: 123,
+				Database: "db", Schema: "schema",
+			},
+			err: ErrEmptyAccount,
+		},
+	}
+	for _, test := range testcases {
+		cfg, err := ParseDSN(test.dsn)
+		switch {
+		case test.err == nil:
+			if err != nil {
+				t.Fatalf("Failed to parse the DSN: %v", err)
+			}
+			if test.config.Account != cfg.Account {
+				t.Fatalf("Failed to match account. expected: %v, got: %v",
+					test.config.Account, cfg.Account)
+			}
+			if test.config.User != cfg.User {
+				t.Fatalf("Failed to match user. expected: %v, got: %v",
+					test.config.User, cfg.User)
+			}
+			if test.config.Password != cfg.Password {
+				t.Fatalf("Failed to match password. expected: %v, got: %v",
+					test.config.Password, cfg.Password)
+			}
+			if test.config.Database != cfg.Database {
+				t.Fatalf("Failed to match database. expected: %v, got: %v",
+					test.config.Database, cfg.Database)
+			}
+			if test.config.Schema != cfg.Schema {
+				t.Fatalf("Failed to match schema. expected: %v, got: %v",
+					test.config.Schema, cfg.Schema)
+			}
+			if test.config.Warehouse != cfg.Warehouse {
+				t.Fatalf("Failed to match warehouse. expected: %v, got: %v",
+					test.config.Warehouse, cfg.Warehouse)
+			}
+			if test.config.Role != cfg.Role {
+				t.Fatalf("Failed to match role. expected: %v, got: %v",
+					test.config.Role, cfg.Role)
+			}
+			if test.config.Protocol != cfg.Protocol {
+				t.Fatalf("Failed to match protocol. expected: %v, got: %v",
+					test.config.Protocol, cfg.Protocol)
+			}
+			if test.config.Passcode != cfg.Passcode {
+				t.Fatalf("Failed to match passcode. expected: %v, got: %v",
+					test.config.Passcode, cfg.Passcode)
+			}
+			if test.config.PasscodeInPassword != cfg.PasscodeInPassword {
+				t.Fatalf("Failed to match passcodeInPassword. expected: %v, got: %v",
+					test.config.PasscodeInPassword, cfg.PasscodeInPassword)
+			}
+		case test.err != nil:
+			if err != test.err {
+				t.Fatalf("Wrong error. expected: %v, got: %v", test.err, err)
+			}
+		}
+	}
+}
