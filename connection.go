47b4fc53f9fe0846b656c298333041ce99557c62
howryu <yuhaowei@umich.edu>
2017-04-24 22:11:16 -0700

    Log close session error instead of returning error

--- connection.go
+++ connection.go
@@ -112,8 +112,12 @@ func (sc *snowflakeConn) Begin() (driver.Tx, error) {
 }
 func (sc *snowflakeConn) Close() (err error) {
 	glog.V(2).Infoln("Close")
+	err = sc.rest.closeSession()
+	if err != nil {
+		glog.Warning(err)
+	}
 	glog.Flush() // must flush log buffer while the process is running.
-	return sc.rest.closeSession()
+	return nil
 }
 func (sc *snowflakeConn) Prepare(query string) (driver.Stmt, error) {
 	glog.V(2).Infoln("Prepare")
