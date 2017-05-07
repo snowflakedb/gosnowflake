0f4493880296aae7a0a63a195da3f457d2a44a56
Shige Takeda <smtakeda@gmail.com>
2017-05-07 08:47:26 -0700

    tests all transports

--- ocsp_test.go
+++ ocsp_test.go
@@ -15,24 +15,33 @@ import (
 func TestOCSP(t *testing.T) {
 	targetURL := "https://sfctest0.snowflakecomputing.com/" // testaccount
 
-	c := &http.Client{
-		Transport: SnowflakeTransportTest,
-		Timeout:   30 * time.Second,
+	transports := []*http.Transport{
+		snowflakeInsecureTransport,
+		SnowflakeTransportSerial,
+		snowflakeTransport,
 	}
-	req, err := http.NewRequest("GET", targetURL, bytes.NewReader(nil))
-	if err != nil {
-		t.Fatalf("fail to create a request. err: %v", err)
-	}
-	res, err := c.Do(req)
-	if err != nil {
-		t.Fatalf("failed to GET contents. err: %v", err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		t.Fatalf("failed to get 200: %v", res.StatusCode)
-	}
-	_, err = ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Fatalf("failed to read content body for %v", targetURL)
+
+	for _, tr := range transports {
+		c := &http.Client{
+			Transport: tr,
+			Timeout:   30 * time.Second,
+		}
+		req, err := http.NewRequest("GET", targetURL, bytes.NewReader(nil))
+		if err != nil {
+			t.Fatalf("fail to create a request. err: %v", err)
+		}
+		res, err := c.Do(req)
+		if err != nil {
+			t.Fatalf("failed to GET contents. err: %v", err)
+		}
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			t.Fatalf("failed to get 200: %v", res.StatusCode)
+		}
+		_, err = ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Fatalf("failed to read content body for %v", targetURL)
+		}
+
 	}
 }
