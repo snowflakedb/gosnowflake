99c3f37e4d893798dacefc4cf98eddcd5778c8cd
Shige Takeda <smtakeda@gmail.com>
2017-05-17 14:34:40 -0700

    more tests for auth.go

--- auth_test.go
+++ auth_test.go
@@ -5,6 +5,9 @@
 package gosnowflake
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/url"
 	"testing"
 	"time"
@@ -68,7 +71,75 @@ func postAuthSuccessWithErrorCode(_ *snowflakeRestful, _ *url.Values, _ map[stri
 	}, nil
 }
 
+func postAuthSuccessWithInvalidErrorCode(_ *snowflakeRestful, _ *url.Values, _ map[string]string, _ []byte, _ time.Duration) (*authResponse, error) {
+	return &authResponse{
+		Success: false,
+		Code:    "abcdef",
+		Message: "wrong!",
+	}, nil
+}
+
 func postAuthSuccess(_ *snowflakeRestful, _ *url.Values, _ map[string]string, _ []byte, _ time.Duration) (*authResponse, error) {
+	return &authResponse{
+		Success: true,
+		Data: authResponseMain{
+			Token:       "t",
+			MasterToken: "m",
+			SessionInfo: authResponseSessionInfo{
+				DatabaseName: "dbn",
+			},
+		},
+	}, nil
+}
+
+func postAuthCheckSAMLResponse(_ *snowflakeRestful, _ *url.Values, _ map[string]string, jsonBody []byte, _ time.Duration) (*authResponse, error) {
+	var ar authRequest
+	if err := json.Unmarshal(jsonBody, &ar); err != nil {
+		return nil, err
+	}
+	if ar.Data.RawSAMLResponse == "" {
+		return nil, errors.New("SAML response is empty")
+	}
+	return &authResponse{
+		Success: true,
+		Data: authResponseMain{
+			Token:       "t",
+			MasterToken: "m",
+			SessionInfo: authResponseSessionInfo{
+				DatabaseName: "dbn",
+			},
+		},
+	}, nil
+}
+
+func postAuthCheckPasscode(_ *snowflakeRestful, _ *url.Values, _ map[string]string, jsonBody []byte, _ time.Duration) (*authResponse, error) {
+	var ar authRequest
+	if err := json.Unmarshal(jsonBody, &ar); err != nil {
+		return nil, err
+	}
+	if ar.Data.Passcode != "987654321" || ar.Data.ExtAuthnDuoMethod != "passcode" {
+		return nil, fmt.Errorf("passcode didn't match. expected: 987654321, got: %v, duo: %v", ar.Data.Passcode, ar.Data.ExtAuthnDuoMethod)
+	}
+	return &authResponse{
+		Success: true,
+		Data: authResponseMain{
+			Token:       "t",
+			MasterToken: "m",
+			SessionInfo: authResponseSessionInfo{
+				DatabaseName: "dbn",
+			},
+		},
+	}, nil
+}
+
+func postAuthCheckPasscodeInPassword(_ *snowflakeRestful, _ *url.Values, _ map[string]string, jsonBody []byte, _ time.Duration) (*authResponse, error) {
+	var ar authRequest
+	if err := json.Unmarshal(jsonBody, &ar); err != nil {
+		return nil, err
+	}
+	if ar.Data.Passcode != "" || ar.Data.ExtAuthnDuoMethod != "passcode" {
+		return nil, fmt.Errorf("passcode must be empty, got: %v, duo: %v", ar.Data.Passcode, ar.Data.ExtAuthnDuoMethod)
+	}
 	return &authResponse{
 		Success: true,
 		Data: authResponseMain{
@@ -135,6 +206,14 @@ func TestUnitAuthenticate(t *testing.T) {
 	if !ok || driverErr.Number != 98765 {
 		t.Fatalf("Snowflake error is expected. err: %v", driverErr)
 	}
+	sr.FuncPostAuth = postAuthSuccessWithInvalidErrorCode
+	_, err = authenticate(
+		sr, "u", "p", "a", "d",
+		"s", "w", "r", "", false,
+		"testapp", []byte{}, "", "")
+	if err == nil {
+		t.Fatal("should have failed.")
+	}
 	sr.FuncPostAuth = postAuthSuccess
 	var resp *authResponseSessionInfo
 	resp, err = authenticate(
@@ -146,5 +225,41 @@ func TestUnitAuthenticate(t *testing.T) {
 	}
 	if resp.DatabaseName != "dbn" {
 		t.Fatalf("failed to get response from auth")
+	}
+}
+
+func TestUnitAuthenticateSaml(t *testing.T) {
+	var err error
+	sr := &snowflakeRestful{
+		FuncPostAuth: postAuthCheckSAMLResponse,
+	}
+	_, err = authenticate(
+		sr, "u", "p", "a", "d",
+		"s", "w", "r", "", false,
+		"testapp", []byte("HTML data in bytes from"), "", "")
+	if err != nil {
+		t.Fatalf("failed to run. err: %v", err)
+	}
+}
+
+func TestUnitAuthenticatePasscode(t *testing.T) {
+	var err error
+	sr := &snowflakeRestful{
+		FuncPostAuth: postAuthCheckPasscode,
+	}
+	_, err = authenticate(
+		sr, "u", "p", "a", "d",
+		"s", "w", "r", "987654321", false,
+		"testapp", []byte{}, "", "")
+	if err != nil {
+		t.Fatalf("failed to run. err: %v", err)
+	}
+	sr.FuncPostAuth = postAuthCheckPasscodeInPassword
+	_, err = authenticate(
+		sr, "u", "p", "a", "d",
+		"s", "w", "r", "987654321", true,
+		"testapp", []byte{}, "", "")
+	if err != nil {
+		t.Fatalf("failed to run. err: %v", err)
 	}
 }
