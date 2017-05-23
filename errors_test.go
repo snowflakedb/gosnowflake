10a6527092d2651dc3dc1c2f1c665aa566f25b89
Shige Takeda <smtakeda@gmail.com>
2017-05-23 10:09:23 -0700

    added more errors test cases

--- errors_test.go
+++ errors_test.go
@@ -15,6 +15,9 @@ func TestErrorMessage(t *testing.T) {
 		Number:  1,
 		Message: "test message",
 	}
+	if !strings.Contains(e.Error(), "000001") {
+		t.Errorf("failed to format error. %v", e)
+	}
 	if !strings.Contains(e.Error(), "test message") {
 		t.Errorf("failed to format error. %v", e)
 	}
@@ -23,7 +26,63 @@ func TestErrorMessage(t *testing.T) {
 		Message:     "test message: %v, %v",
 		MessageArgs: []interface{}{"C1", "C2"},
 	}
+	if !strings.Contains(e.Error(), "000001") {
+		t.Errorf("failed to format error. %v", e)
+	}
 	if !strings.Contains(e.Error(), "test message") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	if !strings.Contains(e.Error(), "C1") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	e = &SnowflakeError{
+		Number:      1,
+		Message:     "test message: %v, %v",
+		MessageArgs: []interface{}{"C1", "C2"},
+		SQLState:    "01112",
+	}
+	if !strings.Contains(e.Error(), "000001") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	if !strings.Contains(e.Error(), "test message") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	if !strings.Contains(e.Error(), "C1") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	if !strings.Contains(e.Error(), "01112") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	e = &SnowflakeError{
+		Number:      1,
+		Message:     "test message: %v, %v",
+		MessageArgs: []interface{}{"C1", "C2"},
+		SQLState:    "01112",
+		QueryID:     "abcdef-abcdef-abcdef",
+	}
+	if !strings.Contains(e.Error(), "000001") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	if !strings.Contains(e.Error(), "test message") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	if !strings.Contains(e.Error(), "C1") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	if !strings.Contains(e.Error(), "01112") {
+		t.Errorf("failed to format error. %v", e)
+	}
+	if strings.Contains(e.Error(), "abcdef-abcdef-abcdef") {
+		// no quid
+		t.Errorf("failed to format error. %v", e)
+	}
+	se, ok := e.(*SnowflakeError)
+	if !ok {
+		t.Errorf("Failed to cast to SnowflakeError. %T, %v", e, e)
+	}
+	se.IncludeQueryID = true
+	if !strings.Contains(e.Error(), "abcdef-abcdef-abcdef") {
+		// no quid
 		t.Errorf("failed to format error. %v", e)
 	}
 }
