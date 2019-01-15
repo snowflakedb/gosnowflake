d95fae461c48f5f0932e843b1c2b919537983895
Tianyi Chen <tianyic933@gmail.com>
2019-01-15 18:24:51 +0000

    address comments

--- parameter_setup.sh
+++ parameter_setup.sh
@@ -1,6 +1,2 @@
 #!/bin/bash -ex
-if [[ -n "$SNOWFLAKE_AZURE" ]]; then
-    openssl aes-256-cbc -k "$super_azure_secret_password" -in parameters_azure.json.enc -out parameters.json -d
-else
-    openssl aes-256-cbc -k "$super_secret_password" -in parameters.json.enc -out parameters.json -d
-fi
+
