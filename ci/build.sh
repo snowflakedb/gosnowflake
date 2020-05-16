78faa3b013108bc9bc6754566ff6d8a812abc5e3
Shige Takeda <shige.takeda@snowflake.com>
2020-05-16 16:51:38 -0700

    add github workflow

---
+++ ci/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Build and Test Golang driver
+#
+set -e
+set -o pipefail
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $THIS_DIR/setup.sh
+cd $THIS_DIR/..
+make fmt lint wss test
