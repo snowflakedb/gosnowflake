7334396af05ac7e206cd0820e108eaac0f3bd91a
Shige Takeda <shige.takeda@snowflake.com>
2020-05-16 18:23:49 -0700

    passphrase

--- ci/scripts/setup.sh
+++ ci/scripts/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 if [[ -n "$SNOWFLAKE_AZURE" ]]; then
-    gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output parameters.json .github/workflows/parameters_azure.json.gpg
+    gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output parameters.json .github/workflows/parameters_azure.json.gpg
 elif [[ -n "$SNOWFLAKE_GCP" ]]; then
-    gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output parameters.json .github/workflows/parameters_gcp.json.gpg
+    gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output parameters.json .github/workflows/parameters_gcp.json.gpg
 else
-    gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output parameters.json .github/workflows/parameters_aws.json.gpg
+    gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output parameters.json .github/workflows/parameters_aws.json.gpg
 fi
 # TODO: openssl aes-256-cbc -k "$super_secret_password" -in rsa-2048-private-key.p8.enc -out rsa-2048-private-key.p8 -d
