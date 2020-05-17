b8c8a7915f4134a8f7bdb94894cc10e2bf7598b4
Shige Takeda <shige.takeda@snowflake.com>
2020-05-16 18:12:26 -0700

    fixed path

--- ci/build.sh
+++ ci/build.sh
@@ -6,6 +6,6 @@ set -e
 set -o pipefail
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $THIS_DIR/setup.sh
+source $THIS_DIR/scripts/setup.sh
 cd $THIS_DIR/..
 make fmt lint wss test
