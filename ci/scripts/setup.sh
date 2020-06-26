734e76b6fc8521f053e2037f7d8056bfc9c239ac
Shige Takeda <shige.takeda@snowflake.com>
2020-06-25 18:45:54 -0700

    SNOW-170948 update credentials (#313)

--- ci/scripts/setup.sh
+++ ci/scripts/setup.sh
@@ -4,11 +4,11 @@
 #
 CI_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "$CLOUD_PROVIDER" == "AZURE" ]]; then
-    PARAMETER_FILE=parameters_azure.json.gpg
+    PARAMETER_FILE=parameters_azure_golang.json.gpg
 elif [[ "$CLOUD_PROVIDER" == "GCP" ]]; then
-    PARAMETER_FILE=parameters_gcp.json.gpg
+    PARAMETER_FILE=parameters_gcp_golang.json.gpg
 else
-    PARAMETER_FILE=parameters_aws.json.gpg
+    PARAMETER_FILE=parameters_aws_golang.json.gpg
 fi
 gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output $CI_SCRIPTS_DIR/../../parameters.json $CI_SCRIPTS_DIR/../../.github/workflows/$PARAMETER_FILE
 gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output $CI_SCRIPTS_DIR/../../rsa-2048-private-key.p8 $CI_SCRIPTS_DIR/../../.github/workflows/rsa-2048-private-key.p8.gpg
