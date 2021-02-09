5ab9a5f9c96c8c582c070880de7a348466681026
Chunhui Shi <chunhui.shi@snowflake.com>
2021-02-09 16:12:18 +0000

    SNOW-212951 Fetch query result with query Id
    When include queryId in context, check the correspondent query's status, then fetch its query result if there is result.

--- rows.go
+++ rows.go
@@ -28,12 +28,13 @@ var (
 )
 
 type snowflakeRows struct {
-	sc              *snowflakeConn
-	ChunkDownloader chunkDownloader
-	queryID         string
-	status          queryStatus
-	err             error
-	errChannel      chan error
+	sc                  *snowflakeConn
+	ChunkDownloader     chunkDownloader
+	tailChunkDownloader chunkDownloader
+	queryID             string
+	status              queryStatus
+	err                 error
+	errChannel          chan error
 }
 
 type snowflakeValue interface{}
@@ -207,5 +208,16 @@ func (rows *snowflakeRows) waitForAsyncQueryStatus() error {
 	} else if rows.status == QueryFailed {
 		return rows.err
 	}
+	return nil
+}
+
+func (rows *snowflakeRows) addDownloader(newDL chunkDownloader) error {
+	if rows.ChunkDownloader == nil {
+		rows.ChunkDownloader = newDL
+		rows.tailChunkDownloader = newDL
+		return nil
+	}
+	rows.tailChunkDownloader.setNextChunkDownloader(newDL)
+	rows.tailChunkDownloader = newDL
 	return nil
 }
