bbabbe5ffa003a1cc712cdd1e51c9ac78c5aaed8
Xuewei Zhang <xuewei@observeinc.com>
2021-04-26 14:53:17 -0700

    snowflakeRows: do not marshall ChunkDownloader in easyjson
    This commit is adapted from:
    ac0a31e Updated easyjson version and regenerated easyjson marshalling code
        Change-Id: I9c22e7ad8a0846001f29c1e8ae4922ef0c261389

--- rows.go
+++ rows.go
@@ -30,7 +30,7 @@ var (
 
 type snowflakeRows struct {
 	sc                  *snowflakeConn
-	ChunkDownloader     chunkDownloader
+	ChunkDownloader     chunkDownloader `json:"-"`
 	execResp            *execResponse
 	tailChunkDownloader chunkDownloader
 	queryID             string
