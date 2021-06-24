a65e83ca5cd7931f10a9491d2c8f5a07481b2995
Aswin Karumbunathan <aswin@observeinc.com>
2021-06-24 17:35:36 +0000

    parameters.sh: Add ability to specify context
    Allow the context to be passed in as an argument, in case the hardcoded
    value does not match the user's context that they use for eng.
    
    Change-Id: I724a65a9d40e2eb8eba3757aaf92ef0ee897caa8

--- parameters.sh
+++ parameters.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+context=$1
+if [ -z "$context" ]; then
+    context="k8s.observe-eng.com"
+fi
 echo -n '{"testconnection":{'
 for E in SNOWFLAKE_TEST_ACCOUNT SNOWFLAKE_TEST_DATABASE SNOWFLAKE_TEST_WAREHOUSE SNOWFLAKE_TEST_USER SNOWFLAKE_TEST_ROLE SNOWFLAKE_TEST_PASSWORD; do
-	echo -n $COMMA\"$E\":\"`kubectl --context=k8s.observe-eng.com -n eng get secret snowflake-sfdrivertest-credentials -o=jsonpath={.data.$E} | base64 --decode`\"
+	echo -n $COMMA\"$E\":\"`kubectl --context=$context -n eng get secret snowflake-sfdrivertest-credentials -o=jsonpath={.data.$E} | base64 --decode`\"
 	COMMA=,
 done
 echo }}
