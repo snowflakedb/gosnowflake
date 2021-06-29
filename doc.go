4865771f27f18489184e0e85e0ae08d4cd8d38bf
SnowflakeMarkGilkey <mark.gilkey@snowflake.com>
2021-06-29 12:30:50 -0700

    SNOW-373921: Update to use new account name format that is compatible with Snowflake's Organizations feature.

--- doc.go
+++ doc.go
@@ -10,7 +10,7 @@ Clients can use the database/sql package directly. For example:
 	)
 
 	func main() {
-		db, err := sql.Open("snowflake", "user:password@myaccount/mydb")
+		db, err := sql.Open("snowflake", "user:password@my_organization-my_account/mydb")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -24,7 +24,13 @@ Use the Open() function to create a database handle with connection parameters:
 
 	db, err := sql.Open("snowflake", "<connection string>")
 
-The Go Snowflake Driver supports the following connection syntaxes (or data source name (DSN) formats):
+The Go Snowflake Driver supports the following connection syntaxes (or data source name (DSN) formats).
+The first three lines use the newer format (supported by the Organizations feature of Snowflake).
+The last three lines use the older format.
+
+	* username[:password]@<organization_name>-<account_name>/dbname/schemaname[?param1=value&...&paramN=valueN]
+	* username[:password]@<organization_name>-<account_name>/dbname/[?param1=value&...&paramN=valueN]
+	* username[:password]@hostname:port/dbname/schemaname?account=<organization_name>-<account_name>[&param1=value&...&paramN=valueN]
 
 	* username[:password]@accountname/dbname/schemaname[?param1=value&...&paramN=valueN]
 	* username[:password]@accountname/dbname[?param1=value&...&paramN=valueN]
@@ -33,10 +39,10 @@ The Go Snowflake Driver supports the following connection syntaxes (or data sour
 where all parameters must be escaped or use Config and DSN to construct a DSN string.
 
 The following example opens a database handle with the Snowflake account
-myaccount where the username is jsmith, password is mypassword, database is
+my_organization-my_account where the username is jsmith, password is mypassword, database is
 mydb, schema is testschema, and warehouse is mywh:
 
-	db, err := sql.Open("snowflake", "jsmith:mypassword@myaccount/mydb/testschema?warehouse=mywh")
+	db, err := sql.Open("snowflake", "jsmith:mypassword@my_organization-my_account/mydb/testschema?warehouse=mywh")
 
 Connection Parameters
 
@@ -46,11 +52,17 @@ The connection string (DSN) can contain both connection parameters (described be
 The following connection parameters are supported:
 
 	* account <string>: Specifies the name of your Snowflake account, where string is the name
-		assigned to your account by Snowflake. In the URL you received from
-		Snowflake, your account name is the first segment in the domain (e.g.
-		abc123 in https://abc123.snowflakecomputing.com). This parameter is
-		optional if your account is specified after the @ character. If you
-		are not on us-west-2 region or AWS deployment, then append the region
+		assigned to your account by Snowflake.
+
+		For newer accounts, and for older accounts converted to use the Organizations feature,
+		your full account name is your organization name followed by a dash followed by the
+		name of your account within your organization. For example: "my_organization-my_account".
+
+		For other accounts, your account name is the first segment in the domain in the URL that
+		you received from Snowflake (e.g. "abc123" in "https://abc123.snowflakecomputing.com").
+
+		For accounts using the older account naming convention ONLY:
+		If you are not on us-west-2 region or AWS deployment, then append the region
 		after the account name, e.g. "<account>.<region>". If you are not on
 		AWS deployment, then append not only the region, but also the platform
 		(e.g. "<account>.<region>.<platform>"). Account, region, and platform
@@ -58,6 +70,8 @@ The following connection parameters are supported:
 		a global url, then append connection group and "global"
 		(e.g., "account-<connection_group>.global"). Account and connection group are
 		separated by a dash ("-"), as shown above.
+
+		This parameter is optional if your account is specified after the @ character.
 
 	* region <string>: DEPRECATED. You may specify a region, such as
 		"eu-central-1", with this parameter. However, since this parameter
