31161adc8100621333ab8ac121f810841951a26d
Agam Brahma <agam@sigmacomputing.com>
2021-11-04 01:03:13 -0700

    [Fix] shutdown race over access of the connection's "restful" structure.

--- connection.go
+++ connection.go
@@ -64,6 +64,7 @@ type snowflakeConn struct {
 	ctx             context.Context
 	cfg             *Config
 	rest            *snowflakeRestful
+	restMu          sync.RWMutex // guard shutdown race
 	SequenceCounter uint64
 	QueryID         string
 	SQLState        string
@@ -224,6 +225,8 @@ func (sc *snowflakeConn) BeginTx(
 
 func (sc *snowflakeConn) cleanup() {
 	// must flush log buffer while the process is running.
+	sc.restMu.Lock()
+	defer sc.restMu.Unlock()
 	if sc.rest != nil && sc.rest.Client != nil {
 		sc.rest.Client.CloseIdleConnections()
 	}
