ef8f48826bf105bcefcfc19430ccfa3bdf8f3896
Agam Brahma <agam.brahma+git@gmail.com>
2021-11-15 17:33:51 -0800

    Add more ways to get _some_ logging for failed requests (#36)
    * Add more ways to get _some_ logging for failed requests
    
    (if request-body is consumed, still dump the sampled response body if we can)
    
    Co-authored-by: Agam Brahma <agam@sigmacomputing.com>

--- retry.go
+++ retry.go
@@ -262,9 +262,14 @@ func (r *retryHTTP) execute() (res *http.Response, err error) {
 				break
 			}
 			sampledResponseBody = getBodyTrace(r.ctx, res)
-			if shouldDumpResponseInfo && res.Request != nil {
-				logger.WithContext(r.ctx).Warningf(
-					"failed http connection. HTTP Status: %v. headers %v. request %v. body %s. retrying...\n", res.StatusCode, res.Header, res.Request.URL, sampledResponseBody)
+			if shouldDumpResponseInfo {
+				if res.Request != nil {
+					logger.WithContext(r.ctx).Warningf(
+						"failed http connection. HTTP Status: %v. headers %v. request %v. body %s. retrying...\n", res.StatusCode, res.Header, res.Request.URL, sampledResponseBody)
+				} else {
+					logger.WithContext(r.ctx).Warningf(
+						"failed http connection. HTTP Status: %v. headers %v. body %s. retrying...\n", res.StatusCode, res.Header, sampledResponseBody)
+				}
 			} else {
 				logger.WithContext(r.ctx).Warningf(
 					"failed http connection. HTTP Status: %v. retrying...\n", res.StatusCode)
@@ -283,8 +288,11 @@ func (r *retryHTTP) execute() (res *http.Response, err error) {
 					return nil, err
 				}
 				if res != nil {
-					if shouldDumpResponseInfo && res.Request != nil {
-						return nil, fmt.Errorf("timeout after %s. HTTP Status: %v. Header: %v. request: %v. body: %s. Hanging?", r.timeout, res.StatusCode, res.Header, res.Request.URL, sampledResponseBody)
+					if shouldDumpResponseInfo {
+						if res.Request != nil {
+							return nil, fmt.Errorf("timeout after %s. HTTP Status: %v. Header: %v. request: %v. body: %s. Hanging?", r.timeout, res.StatusCode, res.Header, res.Request.URL, sampledResponseBody)
+						}
+						return nil, fmt.Errorf("timeout after %s. HTTP Status: %v. Header: %v. body: %s. Hanging?", r.timeout, res.StatusCode, res.Header, sampledResponseBody)
 					}
 					return nil, fmt.Errorf("timeout after %s. HTTP Status: %v. Hanging?", r.timeout, res.StatusCode)
 				}
