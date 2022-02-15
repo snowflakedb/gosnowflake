371f4e9383de956a323bc78c3d8e88329e1738f6
Greg Owen <GregOwen@users.noreply.github.com>
2022-02-15 11:06:52 -0800

    [Feature] Allow clients to specify any/null DataType explicitly
    - Allow clients to specify DataTypeNull explicitly (#50)
    
    * first cut of explict DataTypeNull
    
    * make lint
    
    * more tests
    
    - Allow client to specify any data type explicitly (#28)
    
    * dataTypeMode should work for all types
    
    * fix test
    
    * allow explicit binaryType declaration even if we're already using binaryType
    
    * silly attempt with *SnowflakeDataType
    
    * switch from *SnowflakeDataType --> SnowflakeDataType
    
    * bindings_test.go passes
    
    * add test for corner case
    
    * fix lint
    
    * fix null-handling tests
    
    * only need connection.CheckNamedValue
    
    * use checked cast instead of switch on type

--- datatype_test.go
+++ datatype_test.go
@@ -3,34 +3,37 @@
 package gosnowflake
 
 import (
-	"database/sql/driver"
 	"fmt"
 	"testing"
 )
 
 type tcDataTypeMode struct {
-	tp    driver.Value
+	tp    SnowflakeDataType
 	tmode snowflakeType
 	err   error
 }
 
-func TestDataTypeMode(t *testing.T) {
+func TestClientTypeToInternal(t *testing.T) {
 	var testcases = []tcDataTypeMode{
+		{tp: DataTypeFixed, tmode: fixedType, err: nil},
+		{tp: DataTypeReal, tmode: realType, err: nil},
+		{tp: DataTypeText, tmode: textType, err: nil},
+		{tp: DataTypeDate, tmode: dateType, err: nil},
+		{tp: DataTypeVariant, tmode: variantType, err: nil},
 		{tp: DataTypeTimestampLtz, tmode: timestampLtzType, err: nil},
 		{tp: DataTypeTimestampNtz, tmode: timestampNtzType, err: nil},
 		{tp: DataTypeTimestampTz, tmode: timestampTzType, err: nil},
-		{tp: DataTypeDate, tmode: dateType, err: nil},
-		{tp: DataTypeTime, tmode: timeType, err: nil},
+		{tp: DataTypeObject, tmode: objectType, err: nil},
+		{tp: DataTypeArray, tmode: arrayType, err: nil},
 		{tp: DataTypeBinary, tmode: binaryType, err: nil},
-		{tp: DataTypeFixed, tmode: fixedType,
-			err: fmt.Errorf(errMsgInvalidByteArray, DataTypeFixed)},
-		{tp: DataTypeReal, tmode: realType,
-			err: fmt.Errorf(errMsgInvalidByteArray, DataTypeFixed)},
-		{tp: 123, tmode: nullType,
-			err: fmt.Errorf(errMsgInvalidByteArray, 123)},
+		{tp: DataTypeTime, tmode: timeType, err: nil},
+		{tp: DataTypeBoolean, tmode: booleanType, err: nil},
+		{tp: DataTypeNull, tmode: nullType, err: nil},
+		{tp: nil, tmode: nullType,
+			err: fmt.Errorf(errMsgInvalidByteArray, nil)},
 	}
 	for _, ts := range testcases {
-		tmode, err := dataTypeMode(ts.tp)
+		tmode, err := clientTypeToInternal(ts.tp)
 		if ts.err == nil {
 			if err != nil {
 				t.Errorf("failed to get datatype mode: %v", err)
