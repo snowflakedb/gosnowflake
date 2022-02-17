5d0dfb547efff6b2daad4cf10068e907b20aacb1
Yifeng-Sigma <yifeng@sigmacomputing.com>
2022-02-16 18:17:58 -0600

    [Feature] Add fetchers for result, monitoring, query-graph.
    * add hook to fetch "monitoring" data and query-graph, in goroutines
    
    * add support to fetching monitoring info
    
    * add monitoring threshold
    
    * move to Duration
    
    Lint/Build fixes
    
    nit: Goimports fixes
    
    - [Feature] [monitoring] fetch the query graph for slow queries (#26)
    
    * fetch query graph
    
    * fix
    
    * more fixes
    
    update interface
    
    move monitoring to a goroutine; client decides how long it'll wait (#27)
    
    add MonitoringResultFetcher to fetch monitoring (#48)
    
    - not use qid from SFconnection when fetch monitoring data (#52)

--- rows.go
+++ rows.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022 Snowflake Computing Inc. All rights reserved.
+// Copyright (c) 2017-2021 Snowflake Computing Inc. All right reserved.
 
 package gosnowflake
 
@@ -36,6 +36,7 @@ type snowflakeRows struct {
 	status              queryStatus
 	err                 error
 	errChannel          chan error
+	monitoring          *monitoringResult
 }
 
 type snowflakeValue interface{}
@@ -139,6 +140,14 @@ func (rows *snowflakeRows) ColumnTypeScanType(index int) reflect.Type {
 
 func (rows *snowflakeRows) GetQueryID() string {
 	return rows.queryID
+}
+
+func (rows *snowflakeRows) Monitoring(wait time.Duration) *QueryMonitoringData {
+	return rows.monitoring.Monitoring(wait)
+}
+
+func (rows *snowflakeRows) QueryGraph(wait time.Duration) *QueryGraphData {
+	return rows.monitoring.QueryGraph(wait)
 }
 
 func (rows *snowflakeRows) GetStatus() queryStatus {
