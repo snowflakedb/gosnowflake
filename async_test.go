3f10a57ef13d956d2c1526cba8535bf5bfcc469a
Mihai Claudiu Toader <mihai@sigmacomputing.com>
2022-03-11 14:47:50 -0800

    [Fix] SIG-18794: Fix getAsync() to not panic on context exceeded + test. (#56)

--- async_test.go
+++ async_test.go
@@ -4,9 +4,9 @@ package gosnowflake
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestAsyncMode(t *testing.T) {
@@ -125,6 +125,7 @@ func TestMultipleAsyncQueries(t *testing.T) {
 
 		go retrieveRows(rows1, ch1)
 		go retrieveRows(rows2, ch2)
+
 		select {
 		case res := <-ch1:
 			t.Fatalf("value %v should not have been called earlier.", res)
@@ -136,7 +137,75 @@ func TestMultipleAsyncQueries(t *testing.T) {
 	})
 }
 
-func retrieveRows(rows *sql.Rows, ch chan string) {
+func TestMultipleAsyncSuccessAndFailedQueries(t *testing.T) {
+	ctx := WithAsyncMode(context.Background())
+	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	s1 := "foo"
+	s2 := "bar"
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	runTests(t, dsn, func(dbt *DBTest) {
+		rows1 := dbt.mustQueryContext(ctx, fmt.Sprintf("select distinct '%s' from table (generator(timelimit=>3))", s1))
+		defer rows1.Close()
+
+		rows2 := dbt.mustQueryContext(ctx, fmt.Sprintf("select distinct '%s' from table (generator(timelimit=>7))", s2))
+		defer rows2.Close()
+
+		go retrieveRows(rows1, ch1)
+		go retrieveRows(rows2, ch2)
+
+		res1 := <-ch1
+		if res1 != s1 {
+			t.Fatalf("query failed. expected: %v, got: %v", s1, res1)
+		}
+
+		// wait until rows2 is done
+		<-ch2
+		driverErr, ok := rows2.Err().(*SnowflakeError)
+		if !ok || driverErr == nil || driverErr.Number != ErrAsync {
+			t.Fatalf("Snowflake ErrAsync expected. got: %T, %v", rows2.Err(), rows2.Err())
+		}
+	})
+}
+
+func TestMultipleAsyncSuccessAndFailedQueries(t *testing.T) {
+	ctx := WithAsyncMode(context.Background())
+	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	s1 := "foo"
+	s2 := "bar"
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	runTests(t, dsn, func(dbt *DBTest) {
+		rows1 := dbt.mustQueryContext(ctx, fmt.Sprintf("select distinct '%s' from table (generator(timelimit=>3))", s1))
+		defer rows1.Close()
+
+		rows2 := dbt.mustQueryContext(ctx, fmt.Sprintf("select distinct '%s' from table (generator(timelimit=>7))", s2))
+		defer rows2.Close()
+
+		go retrieveRows(rows1, ch1)
+		go retrieveRows(rows2, ch2)
+
+		res1 := <-ch1
+		if res1 != s1 {
+			t.Fatalf("query failed. expected: %v, got: %v", s1, res1)
+		}
+
+		// wait until rows2 is done
+		<-ch2
+		driverErr, ok := rows2.Err().(*SnowflakeError)
+		if !ok || driverErr == nil || driverErr.Number != ErrAsync {
+			t.Fatalf("Snowflake ErrAsync expected. got: %T, %v", rows2.Err(), rows2.Err())
+		}
+	})
+}
+
+func retrieveRows(rows *RowsExtended, ch chan string) {
 	var s string
 	for rows.Next() {
 		if err := rows.Scan(&s); err != nil {
