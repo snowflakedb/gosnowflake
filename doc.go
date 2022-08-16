a18861c2e9541467895b296bd95795f746b3e94d
Lorna Barber <lbarber@magnitude.com>
2022-08-16 15:07:31 -0700

    update doc.go with valid tracing values
    format

--- doc.go
+++ doc.go
@@ -113,6 +113,7 @@ The following connection parameters are supported:
 								 Database, Schema, Warehouse and Role when setting up the connection
 
     * tracing: Specifies the logging level to be used. Set to error by default.
+        Valid values are trace, debug, info, print, warning, error, fatal, panic.
 
 All other parameters are interpreted as session parameters (https://docs.snowflake.com/en/sql-reference/parameters.html).
 For example, the TIMESTAMP_OUTPUT_FORMAT session parameter can be set by adding:
