6c5ea7feb44fa18f2ce9e10c385975283ab51ee0
Eric Bannatyne <aldld@users.noreply.github.com>
2022-09-15 11:24:18 -0700

    [Feature][SIG-24903] Prototype for Snowflake SubmitSync (#81)
    Adds a SubmitSync method to the Snowflake driver that directly calls Snowflake's POST /queries/v1/query-request API endpoint synchronously, waiting for up to 45 seconds (the current, fixed timeout) but does not enter the "ping pong" phase of fetching query results for long-running queries. Instead, the caller is responsible for using the query ID to wait for and fetch query results.
    
    This prototype mainly exists right now so that we can test the relevant functionality and compare performance with the fully asynchronous mode of execution.

--- util.go
+++ util.go
@@ -25,6 +25,7 @@ const (
 	enableHigherPrecision contextKey = "ENABLE_HIGHER_PRECISION"
 	arrowBatches          contextKey = "ARROW_BATCHES"
 	queryTag              contextKey = "QUERY_TAG"
+	submitSync            contextKey = "SUBMIT_SYNC"
 )
 
 const (
@@ -100,6 +101,13 @@ func WithArrowBatches(ctx context.Context) context.Context {
 // parameter on any queries that are run
 func WithQueryTag(ctx context.Context, tag string) context.Context {
 	return context.WithValue(ctx, queryTag, tag)
+}
+
+// WithSubmitSync returns a context that enables execution of a query that waits
+// synchronously for the default timeout (up to 45 seconds), after which the client
+// can poll for status using the query ID.
+func WithSubmitSync(ctx context.Context) context.Context {
+	return context.WithValue(ctx, submitSync, true)
 }
 
 // Get the request ID from the context if specified, otherwise generate one
