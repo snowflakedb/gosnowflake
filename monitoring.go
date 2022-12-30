74d0bd9fa155dd88da10228a76cd7e5427540e30
mansap22 <110418152+mansap22@users.noreply.github.com>
2022-12-29 16:35:00 -0800

    fix incorrect error sending (#114)

--- monitoring.go
+++ monitoring.go
@@ -428,6 +428,12 @@ func (sc *snowflakeConn) blockOnRunningQuery(
 				if err != nil {
 					return err
 				}
+				return (&SnowflakeError{
+					Number:   code,
+					SQLState: resp.Data.SQLState,
+					Message:  err.Error(),
+					QueryID:  resp.Data.QueryID,
+				}).exceptionTelemetry(sc)
 			}
 			if code == -1 {
 				ok, deadline := ctx.Deadline()
@@ -437,12 +443,6 @@ func (sc *snowflakeConn) blockOnRunningQuery(
 					logger.WithContext(ctx).Errorf("sullSnowflakeRestful")
 				}
 			}
-			return (&SnowflakeError{
-				Number:   code,
-				SQLState: resp.Data.SQLState,
-				Message:  err.Error(),
-				QueryID:  resp.Data.QueryID,
-			}).exceptionTelemetry(sc)
 		}
 		return err
 	}
@@ -455,6 +455,12 @@ func (sc *snowflakeConn) blockOnRunningQuery(
 				code = ErrQueryStatus
 				message = fmt.Sprintf("%s: (failed to parse original code: %s: %s)", message, resp.Code, err.Error())
 			}
+			return (&SnowflakeError{
+				Number:   code,
+				SQLState: resp.Data.SQLState,
+				Message:  message,
+				QueryID:  resp.Data.QueryID,
+			}).exceptionTelemetry(sc)
 		}
 		if code == -1 {
 			ok, deadline := ctx.Deadline()
@@ -464,12 +470,6 @@ func (sc *snowflakeConn) blockOnRunningQuery(
 				logger.WithContext(ctx).Errorf("sullSnowflakeRestful")
 			}
 		}
-		return (&SnowflakeError{
-			Number:   code,
-			SQLState: resp.Data.SQLState,
-			Message:  message,
-			QueryID:  resp.Data.QueryID,
-		}).exceptionTelemetry(sc)
 	}
 	return nil
 }
