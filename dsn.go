c0b333ef45e11158932ac558d15435f0372ebf4e
Lorna <115649563+sfc-gh-ext-simba-lb@users.noreply.github.com>
2023-01-24 14:49:44 -0800

    Fix connection caching for MFA and external browser authenticators (#705)
    * make session parameter use boolean instead of string
    
    * fix connection caching
    
    * add linux setting in test

--- dsn.go
+++ dsn.go
@@ -83,8 +83,10 @@ type Config struct {
 
 	Tracing string // sets logging level
 
-	MfaToken string // Internally used to cache the MFA token
-	IDToken  string // Internally used to cache the Id Token for external browser
+	MfaToken                       string     // Internally used to cache the MFA token
+	IDToken                        string     // Internally used to cache the Id Token for external browser
+	ClientRequestMfaToken          ConfigBool // When true the MFA token is cached in the credential manager. True by default in Windows/OSX. False for Linux.
+	ClientStoreTemporaryCredential ConfigBool // When true the ID token is cached in the credential manager. True by default in Windows/OSX. False for Linux.
 }
 
 // ocspMode returns the OCSP mode in string INSECURE, FAIL_OPEN, FAIL_CLOSED
