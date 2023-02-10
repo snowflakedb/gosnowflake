17007f85e76bb7b4fcf7ef6dfcf7aed35f40b272
Madison Chamberlain <46542378+madisonchamberlain@users.noreply.github.com>
2023-02-10 15:09:49 -0800

    fix status

--- monitoring.go
+++ monitoring.go
@@ -137,31 +137,12 @@ func (sc *snowflakeConn) checkQueryStatus(
 	qid string) (
 	*retStatus, error) {
 	var statusResp statusResponse
+
 	err := sc.getMonitoringResult(ctx, "queries", qid, &statusResp)
 	if err != nil {
-		logger.WithContext(ctx).Errorf("failed to get response. err: %v", err)
 		return nil, err
 	}
 
-	headers := make(map[string]string)
-	param := make(url.Values)
-	param.Add(requestGUIDKey, NewUUID().String())
-	if tok, _, _ := sc.rest.TokenAccessor.GetTokens(); tok != "" {
-		headers[headerAuthorizationKey] = fmt.Sprintf(headerSnowflakeToken, tok)
-	}
-	resultPath := fmt.Sprintf("/monitoring/queries/%s", qid)
-	url := sc.rest.getFullURL(resultPath, &param)
-
-	res, err := sc.rest.FuncGet(ctx, sc.rest, url, headers, sc.rest.RequestTimeout)
-	if err != nil {
-		logger.WithContext(ctx).Errorf("failed to get response. err: %v", err)
-		return nil, err
-	}
-	defer res.Body.Close()
-	if err = json.NewDecoder(res.Body).Decode(&statusResp); err != nil {
-		logger.WithContext(ctx).Errorf("failed to decode JSON. err: %v", err)
-		return nil, err
-	}
 	if !statusResp.Success || len(statusResp.Data.Queries) == 0 {
 		logger.WithContext(ctx).Errorf("status query returned not-success or no status returned.")
 		return nil, (&SnowflakeError{
