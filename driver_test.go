ed1fa721e46075e1c2092bfb69e79003da599c22
Till Kolditz <till@observeinc.com>
2023-05-25 19:39:04 +0200

    Fix rebase and some (stupid) linter issues
    The following tests fail (upstream/reabse/both):
    
    both upstream and rebase:
    TestArrowVariousTypes
    TestBindingBinary
    TestBulkArrayBindingInterface
    TestBindingArray
    TestBindingBulkArray
    TestGetQueryStatus
    TestJSONVariousTypes
    TestJWTAuthentication
    
    only upstream:
    TestExecWithServerSideError
    
    only rebase:
    TestPutGetFile
    TestPutGetStream
    TestPutGetGcsDownscopedCredential

--- driver_test.go
+++ driver_test.go
@@ -168,6 +168,7 @@ type DBTest struct {
 }
 
 func (dbt *DBTest) mustQuery(query string, args ...interface{}) (rows *RowsExtended) {
+	dbt.T.Helper()
 	// handler interrupt signal
 	ctx, cancel := context.WithCancel(context.Background())
 	c := make(chan os.Signal, 1)
@@ -199,6 +200,7 @@ func (dbt *DBTest) mustQuery(query string, args ...interface{}) (rows *RowsExten
 }
 
 func (dbt *DBTest) mustQueryContext(ctx context.Context, query string, args ...interface{}) (rows *RowsExtended) {
+	dbt.T.Helper()
 	// handler interrupt signal
 	ctx, cancel := context.WithCancel(ctx)
 	c := make(chan os.Signal, 1)
@@ -234,6 +236,7 @@ func (dbt *DBTest) query(query string, args ...any) (*sql.Rows, error) {
 }
 
 func (dbt *DBTest) mustQueryAssertCount(query string, expected int, args ...interface{}) {
+	dbt.T.Helper()
 	rows := dbt.mustQuery(query, args...)
 	defer rows.Close()
 	cnt := 0
@@ -250,6 +253,7 @@ func (dbt *DBTest) prepare(query string) (*sql.Stmt, error) {
 }
 
 func (dbt *DBTest) fail(method, query string, err error) {
+	dbt.T.Helper()
 	if len(query) > 300 {
 		query = "[query too large to print]"
 	}
@@ -257,10 +261,12 @@ func (dbt *DBTest) fail(method, query string, err error) {
 }
 
 func (dbt *DBTest) mustExec(query string, args ...interface{}) (res sql.Result) {
+	dbt.T.Helper()
 	return dbt.mustExecContext(context.Background(), query, args...)
 }
 
 func (dbt *DBTest) mustExecContext(ctx context.Context, query string, args ...interface{}) (res sql.Result) {
+	dbt.T.Helper()
 	res, err := dbt.conn.ExecContext(ctx, query, args...)
 	if err != nil {
 		dbt.fail("exec context", query, err)
@@ -273,6 +279,7 @@ func (dbt *DBTest) exec(query string, args ...any) (sql.Result, error) {
 }
 
 func (dbt *DBTest) mustDecimalSize(ct *sql.ColumnType) (pr int64, sc int64) {
+	dbt.T.Helper()
 	var ok bool
 	pr, sc, ok = ct.DecimalSize()
 	if !ok {
@@ -282,6 +289,7 @@ func (dbt *DBTest) mustDecimalSize(ct *sql.ColumnType) (pr int64, sc int64) {
 }
 
 func (dbt *DBTest) mustFailDecimalSize(ct *sql.ColumnType) {
+	dbt.T.Helper()
 	var ok bool
 	if _, _, ok = ct.DecimalSize(); ok {
 		dbt.Fatalf("should not return decimal size. %v", ct)
@@ -289,6 +297,7 @@ func (dbt *DBTest) mustFailDecimalSize(ct *sql.ColumnType) {
 }
 
 func (dbt *DBTest) mustLength(ct *sql.ColumnType) (cLen int64) {
+	dbt.T.Helper()
 	var ok bool
 	cLen, ok = ct.Length()
 	if !ok {
@@ -298,6 +307,7 @@ func (dbt *DBTest) mustLength(ct *sql.ColumnType) (cLen int64) {
 }
 
 func (dbt *DBTest) mustFailLength(ct *sql.ColumnType) {
+	dbt.T.Helper()
 	var ok bool
 	if _, ok = ct.Length(); ok {
 		dbt.Fatalf("should not return length. %v", ct)
@@ -305,6 +315,7 @@ func (dbt *DBTest) mustFailLength(ct *sql.ColumnType) {
 }
 
 func (dbt *DBTest) mustNullable(ct *sql.ColumnType) (canNull bool) {
+	dbt.T.Helper()
 	var ok bool
 	canNull, ok = ct.Nullable()
 	if !ok {
@@ -314,6 +325,7 @@ func (dbt *DBTest) mustNullable(ct *sql.ColumnType) (canNull bool) {
 }
 
 func (dbt *DBTest) mustPrepare(query string) (stmt *sql.Stmt) {
+	dbt.T.Helper()
 	stmt, err := dbt.conn.PrepareContext(context.Background(), query)
 	if err != nil {
 		dbt.fail("prepare", query, err)
