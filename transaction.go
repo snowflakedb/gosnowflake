6b408db7f215976e1e4dcfd61d2d2779976be517
Dawid Heyman <dawid.heyman@snowflake.com>
2023-05-26 14:36:59 +0200

    SNOW-828186 Remove code duplication in transaction.go

--- transaction.go
+++ transaction.go
@@ -4,29 +4,47 @@ package gosnowflake
 
 import (
 	"database/sql/driver"
+	"errors"
 )
 
 type snowflakeTx struct {
 	sc *snowflakeConn
 }
 
+type txCommand int
+
+const (
+	commit txCommand = iota
+	rollback
+)
+
+func (cmd txCommand) string() (txStr string, err error) {
+	switch cmd {
+	case commit:
+		return "COMMIT", nil
+	case rollback:
+		return "ROLLBACK", nil
+	}
+	return "", errors.New("unsupported transaction command")
+}
+
 func (tx *snowflakeTx) Commit() (err error) {
-	if tx.sc == nil || tx.sc.rest == nil {
-		return driver.ErrBadConn
-	}
-	_, err = tx.sc.exec(tx.sc.ctx, "COMMIT", false /* noResult */, false /* isInternal */, false /* describeOnly */, nil)
-	if err != nil {
-		return
-	}
-	tx.sc = nil
-	return
+	return tx.execTxCommand(commit)
 }
 
 func (tx *snowflakeTx) Rollback() (err error) {
+	return tx.execTxCommand(rollback)
+}
+
+func (tx *snowflakeTx) execTxCommand(command txCommand) (err error) {
+	txStr, err := command.string()
+	if err != nil {
+		return err
+	}
 	if tx.sc == nil || tx.sc.rest == nil {
 		return driver.ErrBadConn
 	}
-	_, err = tx.sc.exec(tx.sc.ctx, "ROLLBACK", false /* noResult */, false /* isInternal */, false /* describeOnly */, nil)
+	_, err = tx.sc.exec(tx.sc.ctx, txStr, false /* noResult */, false /* isInternal */, false /* describeOnly */, nil)
 	if err != nil {
 		return
 	}
