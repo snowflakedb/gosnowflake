2f53948af5bb409664e713e084dc4484aef63fe4
sureshmula2 <126016714+sureshmula2@users.noreply.github.com>
2023-07-18 12:02:05 -0700

    Adding Changes of sf_stable_master to master branch (#147)
    * Add more visibility in chunk downloader
    
    * Added changes that are on stable branch but not on master
    
    * Added comments to pass lint

--- connection_util.go
+++ connection_util.go
@@ -39,8 +39,10 @@ func (sc *snowflakeConn) stopHeartBeat() {
 	if sc.cfg != nil && !sc.isClientSessionKeepAliveEnabled() {
 		return
 	}
-	if sc.rest != nil && sc.rest.HeartBeat != nil {
-		sc.rest.HeartBeat.stop()
+	if sc.rest.HeartBeat != nil {
+		if sc.rest != nil && sc.rest.HeartBeat != nil {
+			sc.rest.HeartBeat.stop()
+		}
 	}
 }
 
