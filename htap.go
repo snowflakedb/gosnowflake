461b65380496c5ff84cab9ab25314f6558ccbab6
Piotr Fus <piotr.fus@snowflake.com>
2023-08-28 13:40:21 +0200

    SNOW-895534: Add HTAP query context entries to cache (#889)

--- htap.go
+++ htap.go
@@ -1,8 +1,26 @@
 package gosnowflake
 
+import "sync"
+
 type queryContextEntry struct {
 	ID        int   `json:"id"`
 	Timestamp int64 `json:"timestamp"`
 	Priority  int   `json:"priority"`
 	Context   any   `json:"context,omitempty"`
+}
+
+type queryContextCache struct {
+	mutex   *sync.Mutex
+	entries []queryContextEntry
+}
+
+func (qcc *queryContextCache) init() *queryContextCache {
+	qcc.mutex = &sync.Mutex{}
+	return qcc
+}
+
+func (qcc *queryContextCache) add(qces ...queryContextEntry) {
+	qcc.mutex.Lock()
+	defer qcc.mutex.Unlock()
+	qcc.entries = append(qcc.entries, qces...)
 }
