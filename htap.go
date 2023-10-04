02f13d1e53a8bc52fd38bf7998c01117115ff008
Joyce Ling <115662568+sfc-gh-ext-simba-jl@users.noreply.github.com>
2023-10-04 13:44:38 -0700

    remove mutex lock in getQueryContextCacheSize

--- htap.go
+++ htap.go
@@ -79,11 +79,7 @@ func (qcc *queryContextCache) prune(size int) {
 }
 
 func (qcc *queryContextCache) getQueryContextCacheSize(sc *snowflakeConn) int {
-	paramsMutex.Lock()
-	sizeStr, ok := sc.cfg.Params[queryContextCacheSizeParamName]
-	paramsMutex.Unlock()
-
-	if ok {
+	if sizeStr, ok := sc.cfg.Params[queryContextCacheSizeParamName]; ok {
 		size, err := strconv.Atoi(*sizeStr)
 		if err != nil {
 			logger.Warnf("cannot parse %v as int as query context cache size: %v", sizeStr, err)
