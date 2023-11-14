82a025e524b11b29ec900d825dcb3d3d71b08a6f
Georgios Giannikis <georgios@observeinc.com>
2023-11-14 10:06:13 +0000

    Rebased to upstream v1.7.0
    The following unit tests fail:
    
        * Both upstream and origin
            TestBindingBulkArray
            TestGetQueryStatus
            TestHTAPOptimizations
            TestHTAPOptimizations/useHtapOptimizations=true
            TestJWTAuthentication
    
        * Only origin
            TestPutGetFile
            TestPutGetStream
            TestPutGetGcsDownscopedCredential

--- ci/scripts/test_component.sh
+++ ci/scripts/test_component.sh
@@ -10,6 +10,13 @@ eval $(jq -r '.testconnection | to_entries | map("export \(.key)=\(.value|tostri
 if [[ -n "$GITHUB_WORKFLOW" ]]; then
 	export SNOWFLAKE_TEST_PRIVATE_KEY=$TOPDIR/rsa-2048-private-key.p8
 fi
+
+# TestCreateCredentialCache has a weird setup where if this
+# file exists, the test fails. Instead of fixing the test
+# and risking conflicts resolutions in the future, rm the 
+# file before we run the tests.
+rm -f ${HOME}/.cache/snowflake/temporary_credential.json
+
 env | grep SNOWFLAKE | grep -v PASS | sort
 cd $TOPDIR
 go test -timeout 30m -race -coverprofile=coverage.txt -covermode=atomic -v . 
