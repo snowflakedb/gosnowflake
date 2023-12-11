6547127f995be7f3838af6f5ec04f5b9352ea112
Piotr Fus <piotr.fus@snowflake.com>
2023-12-11 09:37:55 +0100

    SNOW-988793 Do not read arrow chunk when it is empty for arrow batches

--- chunk_downloader_test.go
+++ chunk_downloader_test.go
@@ -2,6 +2,7 @@ package gosnowflake
 
 import (
 	"context"
+	"database/sql/driver"
 	"testing"
 )
 
@@ -23,6 +24,89 @@ func TestChunkDownloaderDoesNotStartWhenArrowParsingCausesError(t *testing.T) {
 			err := scd.start()
 
 			assertNotNilF(t, err)
+		})
+	}
+}
+
+func TestWithArrowBatchesWhenQueryReturnsNoRowsWhenUsingNativeGoSQLInterface(t *testing.T) {
+	runDBTest(t, func(dbt *DBTest) {
+		var rows driver.Rows
+		var err error
+		err = dbt.conn.Raw(func(x interface{}) error {
+			rows, err = x.(driver.QueryerContext).QueryContext(WithArrowBatches(context.Background()), "SELECT 1 WHERE 0 = 1", nil)
+			return err
+		})
+		assertNilF(t, err)
+		rows.Close()
+	})
+}
+
+func TestWithArrowBatchesWhenQueryReturnsNoRows(t *testing.T) {
+	runDBTest(t, func(dbt *DBTest) {
+		rows := dbt.mustQueryContext(WithArrowBatches(context.Background()), "SELECT 1")
+		defer rows.Close()
+		assertFalseF(t, rows.Next())
+	})
+}
+
+func TestWithArrowBatchesWhenQueryReturnsSomeRowsInGivenFormatUsingNativeGoSQLInterface(t *testing.T) {
+	for _, tc := range []struct {
+		useJSON bool
+		desc    string
+	}{
+		{
+			useJSON: true,
+			desc:    "json",
+		},
+		{
+			useJSON: false,
+			desc:    "arrow",
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			runDBTest(t, func(dbt *DBTest) {
+				if tc.useJSON {
+					dbt.mustExec(forceJSON)
+				}
+				var rows driver.Rows
+				var err error
+				err = dbt.conn.Raw(func(x interface{}) error {
+					rows, err = x.(driver.QueryerContext).QueryContext(WithArrowBatches(context.Background()), "SELECT 1", nil)
+					return err
+				})
+				assertNilF(t, err)
+				defer rows.Close()
+				values := make([]driver.Value, 1)
+				rows.Next(values)
+				assertEqualE(t, values[0], nil)
+			})
+		})
+	}
+}
+
+func TestWithArrowBatchesWhenQueryReturnsSomeRowsInGivenFormat(t *testing.T) {
+	for _, tc := range []struct {
+		useJSON bool
+		desc    string
+	}{
+		{
+			useJSON: true,
+			desc:    "json",
+		},
+		{
+			useJSON: false,
+			desc:    "arrow",
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			runDBTest(t, func(dbt *DBTest) {
+				if tc.useJSON {
+					dbt.mustExec(forceJSON)
+				}
+				rows := dbt.mustQueryContext(WithArrowBatches(context.Background()), "SELECT 1")
+				defer rows.Close()
+				assertFalseF(t, rows.Next())
+			})
 		})
 	}
 }
