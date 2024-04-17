57e8c084ee1b2f403bc29feefc73a53a014a7f95
Efstathios Chouliaris <Efstathios.Chouliaris@thomsonreuters.com>
2024-04-17 09:06:30 -0500

    update apache arrow to  v16

--- converter_test.go
+++ converter_test.go
@@ -17,10 +17,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/apache/arrow/go/v15/arrow"
-	"github.com/apache/arrow/go/v15/arrow/array"
-	"github.com/apache/arrow/go/v15/arrow/decimal128"
-	"github.com/apache/arrow/go/v15/arrow/memory"
+	"github.com/apache/arrow/go/v16/arrow"
+	"github.com/apache/arrow/go/v16/arrow/array"
+	"github.com/apache/arrow/go/v16/arrow/decimal128"
+	"github.com/apache/arrow/go/v16/arrow/memory"
 )
 
 func stringIntToDecimal(src string) (decimal128.Num, bool) {
