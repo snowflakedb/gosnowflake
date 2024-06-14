aa1b20513088660cd61161c2fa68beb7c5c5f309
madisonchamberlain <maddychamberlain@gmail.com>
2024-06-14 13:32:25 -0700

    rename default function to noop

--- function_wrappers.go
+++ function_wrappers.go
@@ -2,11 +2,14 @@ package gosnowflake
 
 import "context"
 
-// Define a type for the function that wraps goroutines
+// GoroutineWrapperFunc is used to wrap goroutines. This is useful if the caller wants
+// to recover panics, rather than letting panics cause a system crash. A suggestion would be to
+// use use the recover functionality, and log the panic as is most useful to you
 type GoroutineWrapperFunc func(ctx context.Context, f func())
 
-// Global variable to hold the function pointer
-var defaultDoesNothing = func(_ context.Context, f func()) {
+// The default GoroutineWrapperFunc; this does nothing. With this default wrapper
+// panics will take down binary as expected
+var noopGoroutineWrapper = func(_ context.Context, f func()) {
 	f()
 }
-var GoroutineWrapper GoroutineWrapperFunc = defaultDoesNothing
+var GoroutineWrapper GoroutineWrapperFunc = noopGoroutineWrapper
