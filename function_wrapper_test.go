489ad057c450e614c36ac7b54222386170d27627
madisonchamberlain <maddychamberlain@gmail.com>
2024-06-20 09:05:26 -0400

    lock the global variable so multiple routines dont read at once

--- function_wrapper_test.go
+++ function_wrapper_test.go
@@ -2,16 +2,21 @@ package gosnowflake
 
 import (
 	"context"
+	"sync"
 	"testing"
 )
 
 var (
-	goWrapperCalled = false
+	goWrapperCalled          = false
+	testGoRoutineWrapperLock sync.Mutex
 )
 
 // this is the go wrapper function we are going to pass into GoroutineWrapper.
 // we will know that this has been called if the channel is closed
 var closeGoWrapperCalledChannel = func(ctx context.Context, f func()) {
+	testGoRoutineWrapperLock.Lock()
+	defer testGoRoutineWrapperLock.Unlock()
+
 	goWrapperCalled = true
 
 	f()
@@ -30,6 +35,8 @@ func TestGoWrapper(t *testing.T) {
 		rows := dbt.mustQueryContext(ctx, "SELECT 1")
 		defer rows.Close()
 
+		testGoRoutineWrapperLock.Lock()
+		defer testGoRoutineWrapperLock.Unlock()
 		assertTrueF(t, goWrapperCalled, "channel should be closed, indicating our wrapper worked")
 	})
 }
