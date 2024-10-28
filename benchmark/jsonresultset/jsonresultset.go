5918207c422078ad781dd53be5dbaffcd48aec8d
Piotr Fus <piotr.fus@snowflake.com>
2024-10-28 13:46:20 +0100

    SNOW-1763529 Remove old benchmarks (#1231)

--- benchmark/jsonresultset/jsonresultset.go
+++
@@ -1,7 +0,0 @@
-// Package jsonresultset is a benchmark for large json result sets
-//
-// This exists to mitigate "no non-test Go files" in the latest Go
-package jsonresultset
-
-func main() {
-}
--- benchmark/largesetresult/largesetresult.go
+++
@@ -1,7 +0,0 @@
-// Package largesetresult is a benchmark for large result sets
-//
-// This exists to mitigate "no non-test Go files" in the latest Go
-package largesetresult
-
-func main() {
-}
