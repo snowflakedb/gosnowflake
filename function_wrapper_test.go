171ddf2540f3a24f2a990e8453dc425ea864a4a0
Piotr Fus <piotr.fus@snowflake.com>
2024-11-28 07:47:13 +0100

    NO-SNOW Fix TestGoWrapper (#1257)

--- function_wrapper_test.go
+++ function_wrapper_test.go
@@ -6,30 +6,30 @@ import (
 	"testing"
 )
 
-var (
-	goWrapperCalled          = false
-	testGoRoutineWrapperLock sync.Mutex
-)
-
-func setGoWrapperCalled(value bool) {
-	testGoRoutineWrapperLock.Lock()
-	defer testGoRoutineWrapperLock.Unlock()
-	goWrapperCalled = value
-}
-func getGoWrapperCalled() bool {
-	testGoRoutineWrapperLock.Lock()
-	defer testGoRoutineWrapperLock.Unlock()
-	return goWrapperCalled
-}
-
-// this is the go wrapper function we are going to pass into GoroutineWrapper.
-// we will know that this has been called if the channel is closed
-var closeGoWrapperCalledChannel = func(ctx context.Context, f func()) {
-	setGoWrapperCalled(true)
-	f()
-}
-
 func TestGoWrapper(t *testing.T) {
+	var (
+		goWrapperCalled          = false
+		testGoRoutineWrapperLock sync.Mutex
+	)
+
+	setGoWrapperCalled := func(value bool) {
+		testGoRoutineWrapperLock.Lock()
+		defer testGoRoutineWrapperLock.Unlock()
+		goWrapperCalled = value
+	}
+	getGoWrapperCalled := func() bool {
+		testGoRoutineWrapperLock.Lock()
+		defer testGoRoutineWrapperLock.Unlock()
+		return goWrapperCalled
+	}
+
+	// this is the go wrapper function we are going to pass into GoroutineWrapper.
+	// we will know that this has been called if the channel is closed
+	var closeGoWrapperCalledChannel = func(ctx context.Context, f func()) {
+		setGoWrapperCalled(true)
+		f()
+	}
+
 	runDBTest(t, func(dbt *DBTest) {
 		oldGoroutineWrapper := GoroutineWrapper
 		t.Cleanup(func() {
@@ -40,7 +40,10 @@ func TestGoWrapper(t *testing.T) {
 
 		ctx := WithAsyncMode(context.Background())
 		rows := dbt.mustQueryContext(ctx, "SELECT 1")
-		defer rows.Close()
+		assertTrueE(t, rows.Next())
+		var i int
+		assertNilF(t, rows.Scan(&i))
+		rows.Close()
 
 		assertTrueF(t, getGoWrapperCalled(), "channel should be closed, indicating our wrapper worked")
 	})
