65e7cddfb7fdf6b40dc1ff6f22dddd95ff4599f6
Patryk Cyrek <patryk.cyrek@snowflake.com>
2024-12-05 10:52:53 +0100

    docker repo setup2

--- ci/test_authentication.sh
+++ ci/test_authentication.sh
@@ -4,6 +4,12 @@ set -o pipefail
 export THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export WORKSPACE=${WORKSPACE:-/tmp}
 
+CI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ -n "$JENKINS_HOME" ]]; then
+  source $CI_DIR/_init.sh
+  source $CI_DIR/scripts/login_internal_docker.sh
+fi
 gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output $THIS_DIR/../.github/workflows/parameters_aws_auth_tests.json "$THIS_DIR/../.github/workflows/parameters_aws_auth_tests.json.gpg"
 
 docker run \
