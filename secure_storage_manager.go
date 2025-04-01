40e4f5c874dcc003f6318a355b66da8056db5ce4
Piotr Fus <piotr.fus@snowflake.com>
2025-04-01 10:55:30 +0200

    SNOW-1825790 Fix error message when credentials file owner does not m… (#1349)

--- secure_storage_manager.go
+++ secure_storage_manager.go
@@ -340,7 +340,7 @@ func (ssm *fileBasedSecureStorageManager) ensurePermissionsAndOwner(f *os.File, 
 		return nil
 	}
 	if strconv.Itoa(int(ownerUID)) != currentUser.Uid {
-		return errors.New("incorrect owner of " + ssm.credDirPath)
+		return errors.New("incorrect owner of " + f.Name())
 	}
 	return nil
 }
