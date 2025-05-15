9914b60fb0e471188919d5c01afe9b11129be56f
Matt Braymer-Hayes <matt.hayes91@gmail.com>
2025-05-15 13:29:24 -0400

    Revert t.Helper()

--- assert_test.go
+++ assert_test.go
@@ -10,125 +10,101 @@ import (
 	"testing"
 )
 
-func assertNilE(t testing.TB, actual any, descriptions ...string) {
-	t.Helper()
+func assertNilE(t *testing.T, actual any, descriptions ...string) {
 	errorOnNonEmpty(t, validateNil(actual, descriptions...))
 }
 
-func assertNilF(t testing.TB, actual any, descriptions ...string) {
-	t.Helper()
+func assertNilF(t *testing.T, actual any, descriptions ...string) {
 	fatalOnNonEmpty(t, validateNil(actual, descriptions...))
 }
 
-func assertNotNilE(t testing.TB, actual any, descriptions ...string) {
-	t.Helper()
+func assertNotNilE(t *testing.T, actual any, descriptions ...string) {
 	errorOnNonEmpty(t, validateNotNil(actual, descriptions...))
 }
 
-func assertNotNilF(t testing.TB, actual any, descriptions ...string) {
-	t.Helper()
+func assertNotNilF(t *testing.T, actual any, descriptions ...string) {
 	fatalOnNonEmpty(t, validateNotNil(actual, descriptions...))
 }
 
-func assertEqualE(t testing.TB, actual any, expected any, descriptions ...string) {
-	t.Helper()
+func assertEqualE(t *testing.T, actual any, expected any, descriptions ...string) {
 	errorOnNonEmpty(t, validateEqual(actual, expected, descriptions...))
 }
 
-func assertEqualF(t testing.TB, actual any, expected any, descriptions ...string) {
-	t.Helper()
+func assertEqualF(t *testing.T, actual any, expected any, descriptions ...string) {
 	fatalOnNonEmpty(t, validateEqual(actual, expected, descriptions...))
 }
 
-func assertEqualIgnoringWhitespaceE(t testing.TB, actual string, expected string, descriptions ...string) {
-	t.Helper()
+func assertEqualIgnoringWhitespaceE(t *testing.T, actual string, expected string, descriptions ...string) {
 	errorOnNonEmpty(t, validateEqualIgnoringWhitespace(actual, expected, descriptions...))
 }
 
-func assertDeepEqualE(t testing.TB, actual any, expected any, descriptions ...string) {
-	t.Helper()
+func assertDeepEqualE(t *testing.T, actual any, expected any, descriptions ...string) {
 	errorOnNonEmpty(t, validateDeepEqual(actual, expected, descriptions...))
 }
 
-func assertNotEqualF(t testing.TB, actual any, expected any, descriptions ...string) {
-	t.Helper()
+func assertNotEqualF(t *testing.T, actual any, expected any, descriptions ...string) {
 	fatalOnNonEmpty(t, validateNotEqual(actual, expected, descriptions...))
 }
 
-func assertNotEqualE(t testing.TB, actual any, expected any, descriptions ...string) {
-	t.Helper()
+func assertNotEqualE(t *testing.T, actual any, expected any, descriptions ...string) {
 	errorOnNonEmpty(t, validateNotEqual(actual, expected, descriptions...))
 }
 
-func assertBytesEqualE(t testing.TB, actual []byte, expected []byte, descriptions ...string) {
-	t.Helper()
+func assertBytesEqualE(t *testing.T, actual []byte, expected []byte, descriptions ...string) {
 	errorOnNonEmpty(t, validateBytesEqual(actual, expected, descriptions...))
 }
 
-func assertTrueF(t testing.TB, actual bool, descriptions ...string) {
-	t.Helper()
+func assertTrueF(t *testing.T, actual bool, descriptions ...string) {
 	fatalOnNonEmpty(t, validateEqual(actual, true, descriptions...))
 }
 
-func assertTrueE(t testing.TB, actual bool, descriptions ...string) {
-	t.Helper()
+func assertTrueE(t *testing.T, actual bool, descriptions ...string) {
 	errorOnNonEmpty(t, validateEqual(actual, true, descriptions...))
 }
 
-func assertFalseF(t testing.TB, actual bool, descriptions ...string) {
-	t.Helper()
+func assertFalseF(t *testing.T, actual bool, descriptions ...string) {
 	fatalOnNonEmpty(t, validateEqual(actual, false, descriptions...))
 }
 
-func assertFalseE(t testing.TB, actual bool, descriptions ...string) {
-	t.Helper()
+func assertFalseE(t *testing.T, actual bool, descriptions ...string) {
 	errorOnNonEmpty(t, validateEqual(actual, false, descriptions...))
 }
 
-func assertStringContainsE(t testing.TB, actual string, expectedToContain string, descriptions ...string) {
-	t.Helper()
+func assertStringContainsE(t *testing.T, actual string, expectedToContain string, descriptions ...string) {
 	errorOnNonEmpty(t, validateStringContains(actual, expectedToContain, descriptions...))
 }
 
-func assertStringContainsF(t testing.TB, actual string, expectedToContain string, descriptions ...string) {
-	t.Helper()
+func assertStringContainsF(t *testing.T, actual string, expectedToContain string, descriptions ...string) {
 	fatalOnNonEmpty(t, validateStringContains(actual, expectedToContain, descriptions...))
 }
 
-func assertEmptyStringE(t testing.TB, actual string, descriptions ...string) {
-	t.Helper()
+func assertEmptyStringE(t *testing.T, actual string, descriptions ...string) {
 	errorOnNonEmpty(t, validateEmptyString(actual, descriptions...))
 }
 
-func assertHasPrefixE(t testing.TB, actual string, expectedPrefix string, descriptions ...string) {
-	t.Helper()
+func assertHasPrefixE(t *testing.T, actual string, expectedPrefix string, descriptions ...string) {
 	errorOnNonEmpty(t, validateHasPrefix(actual, expectedPrefix, descriptions...))
 }
 
-func assertBetweenE(t testing.TB, value float64, min float64, max float64, descriptions ...string) {
-	t.Helper()
+func assertBetweenE(t *testing.T, value float64, min float64, max float64, descriptions ...string) {
 	errorOnNonEmpty(t, validateValueBetween(value, min, max, descriptions...))
 }
 
-func assertBetweenInclusiveE(t testing.TB, value float64, min float64, max float64, descriptions ...string) {
-	t.Helper()
+func assertBetweenInclusiveE(t *testing.T, value float64, min float64, max float64, descriptions ...string) {
 	errorOnNonEmpty(t, validateValueBetweenInclusive(value, min, max, descriptions...))
 }
 
-func assertEmptyE[T any](t testing.TB, actual []T, descriptions ...string) {
-	t.Helper()
+func assertEmptyE[T any](t *testing.T, actual []T, descriptions ...string) {
 	errorOnNonEmpty(t, validateEmpty(actual, descriptions...))
 }
 
-func fatalOnNonEmpty(t testing.TB, errMsg string) {
-	t.Helper()
+func fatalOnNonEmpty(t *testing.T, errMsg string) {
 	if errMsg != "" {
 		t.Fatal(formatErrorMessage(errMsg))
 	}
 }
 
-func errorOnNonEmpty(t testing.TB, errMsg string) {
-	t.Helper()
+func errorOnNonEmpty(t *testing.T, errMsg string) {
 	if errMsg != "" {
 		t.Error(formatErrorMessage(errMsg))
 	}
