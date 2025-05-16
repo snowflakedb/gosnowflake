990dbc9cf2856b255de1e8f5ef5067a29afe4269
Piotr Fus <piotr.fus@snowflake.com>
2025-05-16 12:56:47 +0200

    SNOW-2091309 Fix precision lost for NUMBER(19, 0) by returning big.Int, align scan types and actual types

--- rows_test.go
+++ rows_test.go
@@ -15,6 +15,7 @@ import (
 type RowsExtended struct {
 	rows      *sql.Rows
 	closeChan *chan bool
+	t *testing.T
 }
 
 func (rs *RowsExtended) Close() error {
@@ -39,12 +40,21 @@ func (rs *RowsExtended) Next() bool {
 	return rs.rows.Next()
 }
 
+func (rs *RowsExtended) mustNext() {
+	assertTrueF(rs.t, rs.rows.Next())
+}
+
 func (rs *RowsExtended) NextResultSet() bool {
 	return rs.rows.NextResultSet()
 }
 
 func (rs *RowsExtended) Scan(dest ...interface{}) error {
 	return rs.rows.Scan(dest...)
+}
+
+func (rs *RowsExtended) mustScan(dest ...interface{}) {
+	err := rs.rows.Scan(dest...)
+	assertNilF(rs.t, err)
 }
 
 // test variables
