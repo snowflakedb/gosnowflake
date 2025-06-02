776a7d6a8dce6272264beea17c5e29b6111f149c
Dawid Heyman <dawid.heyman@snowflake.com>
2025-06-02 15:59:21 +0200

    SNOW-1902248: Add Azure WIF authentication support (#1431)

--- auth_wif_test.go
+++ auth_wif_test.go
@@ -245,7 +245,6 @@ func TestGcpIdentityAttestationCreator(t *testing.T) {
 		name                string
 		wiremockMappingPath string
 		expectedSub         string
-		expectedError       error
 	}{
 		{
 			name:                "Successful flow",
@@ -255,22 +254,18 @@ func TestGcpIdentityAttestationCreator(t *testing.T) {
 		{
 			name:                "No GCP credential - http error",
 			wiremockMappingPath: "wif/gcp/http_error.json",
-			expectedError:       nil,
 		},
 		{
 			name:                "invalid issuer claim",
 			wiremockMappingPath: "wif/gcp/invalid_issuer_claim.json",
-			expectedError:       errors.New("unexpected token issuer: https://not.google.com, should be https://accounts.google.com"),
 		},
 		{
 			name:                "missing issuer claim",
 			wiremockMappingPath: "wif/gcp/missing_issuer_claim.json",
-			expectedError:       errors.New("could not extract claims from token: missing issuer claim in JWT token"),
 		},
 		{
 			name:                "unparsable token",
 			wiremockMappingPath: "wif/gcp/unparsable_token.json",
-			expectedError:       errors.New("could not extract claims from token: unable to extract JWT claims from token: token is malformed: token contains an invalid number of segments"),
 		},
 	}
 
@@ -284,18 +279,14 @@ func TestGcpIdentityAttestationCreator(t *testing.T) {
 			wiremock.registerMappings(t, wiremockMapping{filePath: test.wiremockMappingPath})
 			attestation, err := creator.createAttestation()
 
-			if test.expectedError != nil {
-				assertNotNilE(t, err)
-				assertEqualE(t, test.expectedError.Error(), err.Error())
-				assertNilF(t, attestation)
-			} else if test.expectedSub == "" && test.expectedError == nil {
-				assertNilE(t, err)
-				assertNilF(t, attestation)
-			} else {
+			if test.expectedSub != "" {
 				assertNilE(t, err)
 				assertNotNilE(t, attestation)
 				assertEqualE(t, string(gcpWif), attestation.ProviderType)
 				assertEqualE(t, test.expectedSub, attestation.Metadata["sub"])
+			} else {
+				assertNilE(t, err)
+				assertNilF(t, attestation)
 			}
 		})
 	}
@@ -390,5 +381,192 @@ func TestOidcIdentityAttestationCreator(t *testing.T) {
 				assertEqualE(t, test.expectedSub, attestation.Metadata["sub"])
 			}
 		})
+	}
+}
+
+func TestAzureIdentityAttestationCreator(t *testing.T) {
+	tests := []struct {
+		name                string
+		wiremockMappingPath string
+		metadataProvider    *mockAzureAttestationMetadataProvider
+		cfg                 *Config
+		expectedIss         string
+	}{
+		/*
+		 * {
+		 *     "iss": "https://sts.windows.net/fa15d692-e9c7-4460-a743-29f29522229/",
+		 *     "sub": "77213E30-E8CB-4595-B1B6-5F050E8308FD"
+		 * }
+		 */
+		{
+			name:                "Successful flow",
+			wiremockMappingPath: "wif/azure/successful_flow_basic.json",
+			metadataProvider:    azureVMMetadataProvider(),
+			expectedIss:         "https://sts.windows.net/fa15d692-e9c7-4460-a743-29f29522229/",
+		},
+		/*
+		 * {
+		 *     "iss": "https://login.microsoftonline.com/fa15d692-e9c7-4460-a743-29f29522229/",
+		 *     "sub": "77213E30-E8CB-4595-B1B6-5F050E8308FD"
+		 * }
+		 */
+		{
+			name:                "Successful flow v2 issuer",
+			wiremockMappingPath: "wif/azure/successful_flow_v2_issuer.json",
+			metadataProvider:    azureVMMetadataProvider(),
+			expectedIss:         "https://login.microsoftonline.com/fa15d692-e9c7-4460-a743-29f29522229/",
+		},
+		/*
+		 * {
+		 *     "iss": "https://sts.windows.net/fa15d692-e9c7-4460-a743-29f29522229/",
+		 *     "sub": "77213E30-E8CB-4595-B1B6-5F050E8308FD"
+		 * }
+		 */
+		{
+			name:                "Successful flow azure functions",
+			wiremockMappingPath: "wif/azure/successful_flow_azure_functions.json",
+			metadataProvider:    azureFunctionsMetadataProvider(),
+			expectedIss:         "https://sts.windows.net/fa15d692-e9c7-4460-a743-29f29522229/",
+		},
+		/*
+		 * {
+		 *     "iss": "https://login.microsoftonline.com/fa15d692-e9c7-4460-a743-29f29522229/",
+		 *     "sub": "77213E30-E8CB-4595-B1B6-5F050E8308FD"
+		 * }
+		 */
+		{
+			name:                "Successful flow azure functions v2 issuer",
+			wiremockMappingPath: "wif/azure/successful_flow_azure_functions_v2_issuer.json",
+			metadataProvider:    azureFunctionsMetadataProvider(),
+			expectedIss:         "https://login.microsoftonline.com/fa15d692-e9c7-4460-a743-29f29522229/",
+		},
+		/*
+		 * {
+		 *     "iss": "https://sts.windows.net/fa15d692-e9c7-4460-a743-29f29522229/",
+		 *     "sub": "77213E30-E8CB-4595-B1B6-5F050E8308FD"
+		 * }
+		 */
+		{
+			name:                "Successful flow azure functions no client ID",
+			wiremockMappingPath: "wif/azure/successful_flow_azure_functions_no_client_id.json",
+			metadataProvider: &mockAzureAttestationMetadataProvider{
+				identityEndpointValue: wiremock.baseURL() + "/metadata/identity/endpoint/from/env",
+				identityHeaderValue:   "some-identity-header-from-env",
+				clientIDValue:         "",
+			},
+			expectedIss: "https://sts.windows.net/fa15d692-e9c7-4460-a743-29f29522229/",
+		},
+		/*
+		 * {
+		 *     "iss": "https://sts.windows.net/fa15d692-e9c7-4460-a743-29f29522229/",
+		 *     "sub": "77213E30-E8CB-4595-B1B6-5F050E8308FD"
+		 * }
+		 */
+		{
+			name:                "Successful flow azure functions custom Entra resource",
+			wiremockMappingPath: "wif/azure/successful_flow_azure_functions_custom_entra_resource.json",
+			metadataProvider:    azureFunctionsMetadataProvider(),
+			cfg:                 &Config{WorkloadIdentityEntraResource: "api://1111111-2222-3333-44444-55555555"},
+			expectedIss:         "https://sts.windows.net/fa15d692-e9c7-4460-a743-29f29522229/",
+		},
+		/*
+		 * {
+		 *     "iss": "https://not.azure.sts.issuer.com",
+		 *     "sub": "77213E30-E8CB-4595-B1B6-5F050E8308FD"
+		 * }
+		 */
+		{
+			name:                "Invalid issuer claim",
+			wiremockMappingPath: "wif/azure/invalid_issuer_flow.json",
+			metadataProvider:    azureVMMetadataProvider(),
+		},
+		/*
+		 * {
+		 *   "sub": "77213E30-E8CB-4595-B1B6-5F050E8308FD",
+		 * }
+		 */
+		{
+			name:                "Non-json response",
+			wiremockMappingPath: "wif/azure/non_json_response.json",
+			metadataProvider:    azureVMMetadataProvider(),
+		},
+		{
+			name: "Identity endpoint but no identity header",
+			metadataProvider: &mockAzureAttestationMetadataProvider{
+				identityEndpointValue: wiremock.baseURL() + "/metadata/identity/endpoint/from/env",
+				identityHeaderValue:   "",
+				clientIDValue:         "managed-client-id-from-env",
+			},
+		},
+		{
+			name:                "unparsable token",
+			wiremockMappingPath: "wif/azure/unparsable_token.json",
+			metadataProvider:    azureVMMetadataProvider(),
+		},
+		{
+			name:                "HTTP error",
+			metadataProvider:    azureVMMetadataProvider(),
+			wiremockMappingPath: "wif/azure/http_error.json",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.wiremockMappingPath != "" {
+				wiremock.registerMappings(t, wiremockMapping{filePath: test.wiremockMappingPath})
+			}
+			creator := &azureIdentityAttestationCreator{
+				cfg:                              test.cfg,
+				azureMetadataServiceBaseURL:      wiremock.baseURL(),
+				azureAttestationMetadataProvider: test.metadataProvider,
+				workloadIdentityEntraResource:    determineEntraResource(test.cfg),
+			}
+			attestation, err := creator.createAttestation()
+
+			if test.expectedIss != "" {
+				assertNilE(t, err)
+				assertNotNilE(t, attestation)
+				assertEqualE(t, string(azureWif), attestation.ProviderType)
+				assertEqualE(t, test.expectedIss, attestation.Metadata["iss"])
+				assertEqualE(t, "77213E30-E8CB-4595-B1B6-5F050E8308FD", attestation.Metadata["sub"])
+			} else {
+				assertNilE(t, err)
+				assertNilF(t, attestation)
+			}
+		})
+	}
+}
+
+type mockAzureAttestationMetadataProvider struct {
+	identityEndpointValue string
+	identityHeaderValue   string
+	clientIDValue         string
+}
+
+func (m *mockAzureAttestationMetadataProvider) identityEndpoint() string {
+	return m.identityEndpointValue
+}
+
+func (m *mockAzureAttestationMetadataProvider) identityHeader() string {
+	return m.identityHeaderValue
+}
+
+func (m *mockAzureAttestationMetadataProvider) clientID() string {
+	return m.clientIDValue
+}
+
+func azureFunctionsMetadataProvider() *mockAzureAttestationMetadataProvider {
+	return &mockAzureAttestationMetadataProvider{
+		identityEndpointValue: wiremock.baseURL() + "/metadata/identity/endpoint/from/env",
+		identityHeaderValue:   "some-identity-header-from-env",
+		clientIDValue:         "managed-client-id-from-env",
+	}
+}
+
+func azureVMMetadataProvider() *mockAzureAttestationMetadataProvider {
+	return &mockAzureAttestationMetadataProvider{
+		identityEndpointValue: "",
+		identityHeaderValue:   "",
+		clientIDValue:         "",
 	}
 }
