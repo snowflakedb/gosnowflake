fafb708e81bbeaf8ac7114e185010b5e96e7aed7
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-07-10 12:09:24 +0200

    debuglevel

--- chunk_test.go
+++ chunk_test.go
@@ -395,7 +395,7 @@ func TestWithStreamDownloader(t *testing.T) {
 }
 
 func TestWithArrowBatches(t *testing.T) {
-	logger.SetLevel("debug")
+	logger.SetLevel(logger.DebugLevel)
 	runSnowflakeConnTest(t, func(sct *SCTest) {
 		ctx := WithArrowBatches(sct.sc.ctx)
 		numrows := 3000 // approximately 6 ArrowBatch objects
