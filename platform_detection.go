2538f2590c3914ab0d737d8b89a5284d13d26471
Ruslan Savenok <ruslan.savenok@snowflake.com>
2025-10-29 17:32:42 +0100

    Missed 1

--- platform_detection.go
+++ platform_detection.go
@@ -286,7 +286,7 @@ func detectGcpIdentity(ctx context.Context, timeout time.Duration) platformDetec
 		}
 		return platformNotDetected
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 	if resp.StatusCode == http.StatusOK {
 		return platformDetected
 	}
