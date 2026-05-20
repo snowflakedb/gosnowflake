cc2e6e22c56e835f9620c6acd362fcc4b57bc302
sfc-gh-dszmolka <david.szmolka@snowflake.com>
2026-05-20 16:55:07 +0200

    further CI fix attmpts

--- ci/build.sh
+++ ci/build.sh
@@ -8,6 +8,3 @@ set -o pipefail
 CI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $CI_DIR/..
 make fmt lint
-# make lint runs go mod tidy which may update go.mod/go.sum; restore them so
-# that the subsequent gofix.sh dirty-tree check does not fail.
-git checkout go.mod go.sum
